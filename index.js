3b234c8c3604e851b6f3bad54e040b8407edcc85
Shiloh <74449170+shil6h@users.noreply.github.com>
2021-04-13 00:20:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+const Discord = require('discord.js');
 require('dotenv').config();
-const { Client } = require('discord.js');
+const { Client, DiscordAPIError } = require('discord.js');
 const client = new Client();
 const createCaptcha = require('./captcha');
 const fs = require('fs').promises;
-const { token } = require('./config.json');;
+const { token } = require('./config.json');
+const { prefix } = require('./config.json');
 
 
 client.once('ready', () => {
   console.log('Ready! Made by Shiloh#0001');
-  client.user.setActivity(`shiloh sleep`, {
+  client.user.setActivity(`s!help`, {
     type: 'WATCHING'
   });
   
+client.on("message", async message => {
+    const prefix = "$";
+    if (message.content === 'hi') {
+        message.channel.send('yoo');
+    } else if (message.content === 'cutie') {
+        message.channel.send('<@478051668542619670>')
+    } else if (message.content === 'daddy') {
+        message.channel.send('<@478051668542619670>')
+    }
+})
+
+
+
+
 });
 client.on('guildMemberAdd', async member => {
 				const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome'); 
@@ -51,5 +67,64 @@ client.on('guildMemberAdd', async member => {
         console.log(err);
     }
 });
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    else if (message.content === `${prefix}help`) {
+        const embed = new Discord.MessageEmbed()
+        .setColor('#46edf2')
+        .setDescription('**Commands**')
+        .addField('s!setup', 'Get a description on how to setup shil6h')
+        .addField('s!info', 'Get information about shil6h')
+        .addField('s!invite', 'Get the invite for shil6h')
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp()
+
+        return message.channel.send(embed);
+    }
+})
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    else if (message.content === `${prefix}invite`) {
+        const embed = new Discord.MessageEmbed()
+        .setColor('#46edf2')
+        .setDescription('**Invite**')
+        .addField("[Click Here For To Invite Shil6h To Your Server](https://discord.com/oauth2/authorize?client_id=830529009829543976&scope=bot&permissions=0) ")
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp()
+
+        return message.channel.send(embed);
+    }
+})
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    else  if (message.content === `${prefix}setup`) {
+        const embed = new Discord.MessageEmbed()
+        .setColor('#46edf2')
+        .setDescription('**Setup**')
+        .addField('To setup shil6h all you have to do is invite me to your server and from there I will captcha on')
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp()
+        
+        return message.channel.send(embed);
+    }
+})
+
+client.on('message', message => {
+    if(message.author.bot) return;
+    else if (message.content === `${prefix}info`) {
+        const embed = new Discord.MessageEmbed()
+        .setColor('#46edf2')
+        .setTitle('**Information**')
+        .setDescription('A discord bot that allows you to have to verify via captcha to maintain within the server. Failure to do so will result in a kick. If you do not complete the captcha correctly you will get another attempt. There is no limit on attempts and it is case sensitve. This bot is to help preventing your server from getting raided, botted and or fake discord accounts from joining.')
+        .setFooter(`Requested by ${message.author.tag}`)
+        .setTimestamp()
+        
+        return message.channel.send(embed);
+    }
+})
+
 
 client.login(token);
